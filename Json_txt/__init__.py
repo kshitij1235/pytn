eed233980495f0aa5d641f6585a50fe91ddf6913
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-11-06 20:22:04 +0530

    version 1.2.0

--- Json_txt/__init__.py
+++ Json_txt/__init__.py
@@ -1,35 +1,34 @@
-
 import filemod
 import re
 
-
+def load_txt(filename):
+    """loads the latest copy of the txt"""
+    return filemod.reader(filename)
 
 def number_detect(letter):
+    """Detect the nature of letter is 
+    number or not"""
     try:
         int(letter)
         return True
-    except:return False
+    except:
+        return False
 
-def extract_data(filename):
-    try:file = filemod.reader(filename)
-    except:print("File Dont Exist")
-    
+def extract_values(data):
+    """extract the value from txt"""
+    temp = re.findall(r'\d+', data)
+    return list(map(int, temp))
+
+
+def extract_keys(data):
+    """extract keys from txt"""
     filtured_data = []
     joining_letters = ''
- 
     word = []
-    temp = re.findall(r'\d+', file)
-    values= list(map(int, temp))
-    print(values)
-
-    final_dictonary= {}
-
-    for letters in file:
-        if letters == '\n' or letters == ' ' or letters == "{" or letters == "}":
+    for letters in data:
+        if number_detect(letters):
             continue
-        elif number_detect(letters):
-            continue
-        else:
+        elif letters not in ['\n', ' ', "{", "}"]:
             filtured_data.append(letters)
 
     for letters in filtured_data:
@@ -38,14 +37,47 @@ def extract_data(filename):
             joining_letters = ''
             continue
         else:
-            joining_letters = joining_letters+str(letters)
-    
+            joining_letters += str(letters)
+    return word
+
+
+def extract_data(filename):
+    """return a dict of key values from the
+    txt"""
+
+    try:
+        file = filemod.reader(filename)
+    except FileNotFoundError as e:print(e)
+
+    word = extract_keys(filename)
+    values = extract_values(filename)
+
+    final_dictonary = {}
+
     try:
         for index in range(len(word)):
             final_dictonary[word[index]] = values[index]
-    except:
-        if len(word) > len(values):
-            print("value not assigned to ", word[letters], "properly")
-        else:print("Unknown ERROR")
+    except ValueError:
+        if len(word) > len(values) or len(word)<len(values):
+            print("value not assigned to properly")
+        else:
+            print("Unknown ERROR")
     return final_dictonary
 
+
+def edit_data(filename,key, value_to_change):
+
+    """change the data for the txt"""
+    file=filemod.reader(filename)
+    key_=extract_keys(file)
+    values_=extract_values(file)
+    for i in range(len(key_)):
+        if key == key_[i]:
+            values_.pop(i)
+            values_.insert(i,value_to_change)
+    data=''
+    data="{ \n"
+    for i in range(len(key_)):
+        data=data+str(key_[i])+":"+str(values_[i])+"\n"
+    data += "}"
+    filemod.writer(filename,data,"w")
